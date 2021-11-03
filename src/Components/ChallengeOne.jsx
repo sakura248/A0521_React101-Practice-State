ed4ad97fc32e67d14c2b4a31028546260a0d2e27
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2021-11-02 17:16:57 -0700

    first commit

--- src/Components/ChallengeOne.jsx
+++ src/Components/ChallengeOne.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 //import images
+import left from '../Assets/look-left.jpeg'
+import right from '../Assets/look-right.jpeg'
 
 class ChallengeOne extends Component {
   //declare the state here
   state = {
+    src: left,
+    alt: "looking left"
   };
 
   //click left/right button handler goes here
+  changeEyeLeft=()=> {
+    this.setState({
+      src: left,
+      alt: "looking left"
+    })
+  }
+  changeEyeRight=()=> {
+    this.setState({
+      src: right,
+      alt: "looking right"
+    })
+  }
 
   render() {
     return (
@@ -15,12 +31,12 @@ class ChallengeOne extends Component {
         <div className="msg">
           <img
             className="ch1"
-            src=""
-            alt=""
+            src={this.state.src}
+            alt={this.state.alt}
           />
         </div>
-        <button className="btn">⭠</button>
-        <button className="btn">⭢</button>
+        <button className="btn" onClick={this.changeEyeLeft}>⭠</button>
+        <button className="btn" onClick={this.changeEyeRight}>⭢</button>
       </>
     );
   }
--- src/Components/ChallengeTwo.jsx
+++ src/Components/ChallengeTwo.jsx
@@ -13,18 +13,32 @@ const studentList = [
 export default class ChallengeTwo extends Component {
   //declare the states
   state = {
-    arr: []
+    arr: studentList,
+    index:0
   }
 
   //componentDidMount will execute when the page has loaded (this will only run once)
   componentDidMount() { 
     //display the student list after 3 seconds
-  }
+      
+    this.setState({
+      listText: this.state.arr[this.state.index]
+    })
+
+    const makeArr= this.state.arr.map((text,index)=>{
+      return (
+        <li key={index}>
+          {this.state.text}
+        </li>
+      )
+    })
+        
+      }
+    
 
   //random button handler
   randomize = () => {
     //shuffle the array and set the state
-
   }
 
   render() {
@@ -33,7 +47,9 @@ export default class ChallengeTwo extends Component {
         <h2>Challenge 2</h2>
         <div className='msg'>
           <ul>
-            {/* display the list of students by iterating through the array */}
+            {/* display the list of students by iterating through the array */
+              this.makeArr
+            }
           </ul>
         </div>
         <button className='btn large'>Randomize</button>
